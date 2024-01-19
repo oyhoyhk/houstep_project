bb0cec7aa91ad92393d1f2694cd092e9c0272db2
oyhoyhk <oyhoyhk@naver.com>
2024-01-19 22:27:49 +0900

    feat: implement Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,39 @@
 import styled from "@emotion/styled";
+import { ReactComponent as Logo } from "../../assets/Logo.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  return <Container>Home</Container>;
+  const navigator = useNavigate();
+
+  return (
+    <Container>
+      <Logo />
+      <Button onClick={() => navigator("/order")}>주문하러 가기</Button>
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Button = styled.div`
+  width: 172px;
+  height: 88px;
+  background: white;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  margin-top: 41px;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  max-width: 480px;
+  height: 100vh;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  flex-direction: column;
+`;
--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -4,4 +4,14 @@ export default function Order() {
   return <Container>Order</Container>;
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  max-width: 480px;
+  height: 100vh;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  flex-direction: column;
+`;
